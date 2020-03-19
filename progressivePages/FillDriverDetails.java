e0ff039f7819c5d6423c0dcd0a4af14be7cec394
Dev Acharya <dev.acharya286@gmail.com>
2020-03-18 21:46:33 -0500

    added driver details class to progressive project

--- progressivePages/FillDriverDetails.java
+++ progressivePages/FillDriverDetails.java
@@ -1,5 +1,124 @@
 package progressivePages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
 public class FillDriverDetails {
 
+	WebDriver driver;
+
+	public FillDriverDetails(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(how = How.XPATH, using = "//input[@id='DriversAddPniDetails_embedded_questions_list_Gender_M']")
+	public WebElement selectGender;
+
+	@FindBy(how = How.XPATH, using = "//select[@id='DriversAddPniDetails_embedded_questions_list_MaritalStatus']")
+	public WebElement selectMaritalStatus;
+
+	@FindBy(how = How.XPATH, using = "//select[@id='DriversAddPniDetails_embedded_questions_list_HighestLevelOfEducation']")
+	public WebElement selectLevelOfEducation;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='DriversAddPniDetails_embedded_questions_list_EmploymentStatus']")
+	public WebElement selectEmployementStatus;
+
+	@FindBy(how = How.XPATH, using = "//input[@id='DriversAddPniDetails_embedded_questions_list_Occupation']")
+	public WebElement fillOccupation;
+
+	@FindBy(how = How.XPATH, using = "//input[@id='DriversAddPniDetails_embedded_questions_list_SocialSecurityNumber']")
+	public WebElement fillSSN;
+
+	@FindBy(how = How.XPATH, using = "//select[@id='DriversAddPniDetails_embedded_questions_list_PrimaryResidence']")
+	public WebElement selectPrimaryResidence;
+
+	@FindBy(how = How.XPATH, using = "//select[@id='DriversAddPniDetails_embedded_questions_list_HasPriorAddress']")
+	public WebElement selectMovedInLast2Month;
+
+	@FindBy(how = How.XPATH, using = "//select[@id='DriversAddPniDetails_embedded_questions_list_LicenseStatus']")
+	public WebElement selectLicenseStatus;
+
+	@FindBy(how = How.XPATH, using = "//select[@id='DriversAddPniDetails_embedded_questions_list_DriverYearsLicensed']")
+	public WebElement selectYearLicensed;
+
+	@FindBy(how = How.XPATH, using = "//*[@id='DriversAddPniDetails_embedded_questions_list_HasAccidentsOrClaims_N']")
+	public WebElement selectAccidentClaimOrNot;
+
+	@FindBy(how = How.XPATH, using = "//*[@id='DriversAddPniDetails_embedded_questions_list_HasTicketsOrViolations_N']")
+	public WebElement selectTicketVoilationOrNot;
+
+	@FindBy(how = How.XPATH, using = "//loading-button[contains(.,'Continue')]")
+	public WebElement clickContinue;
+
+	public void chooseGender() {
+		selectGender.click();
+	}
+
+	public void chooseMaritalStatus(String maritalStatus) {
+		Select maritalstatus = new Select(selectMaritalStatus);
+		maritalstatus.selectByVisibleText(maritalStatus);
+	}
+
+	public void chooseLevelOfEducation(String education) {
+		Select levelOfEdu = new Select(selectLevelOfEducation);
+		levelOfEdu.selectByVisibleText(education);
+	}
+	
+	public void chooseEmployeementStatus(String employementStatus) {
+		Select driverEmployementStatus = new Select(selectEmployementStatus);
+		driverEmployementStatus.selectByVisibleText(employementStatus);
+	}
+
+	public void driverOccupation(String occupation) {
+		fillOccupation.sendKeys(occupation);
+	}
+
+	public void driverSSN(String ssn) {
+		fillSSN.sendKeys(ssn);
+	}
+
+	public void choosePrimaryResidence(String residence) {
+		Select driverResidence = new Select(selectPrimaryResidence);
+		driverResidence.selectByVisibleText(residence);
+	}
+
+	public void chooseLatestMoved(String latestMoved) {
+		Select driverMovedStatus = new Select(selectMovedInLast2Month);
+		driverMovedStatus.selectByVisibleText(latestMoved);
+	}
+
+	public void chooseLicenseStatus(String licenseStatus) {
+		Select driverLicenseStatus = new Select(selectLicenseStatus);
+		driverLicenseStatus.selectByVisibleText(licenseStatus);
+	}
+
+	public void chooseYearLicensed(String yearLicensed) {
+		Select driverYearLicensed = new Select(selectYearLicensed);
+		driverYearLicensed.selectByVisibleText(yearLicensed);
+	}
+
+	public void clickAccidentClaimOrNot() {
+		selectAccidentClaimOrNot.click();
+	}
+
+	public void clickTicketVoilationOrNot() {
+		selectTicketVoilationOrNot.click();
+	}
+
+	public void clickContinueForNextPage() {
+		clickContinue.click();
+	}
+	
+	public void clickContinueToReviewPage() {
+		clickContinue.click();
+	}
+	
+	public void clickContinueToFinalDetailPage() {
+		clickContinue.click();
+	}
 }
--- progressivePagesTest/GetByQouteTest.java
+++ progressivePagesTest/GetByQouteTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import progressivePages.EnterZipCode;
+import progressivePages.FillDriverDetails;
 import progressivePages.FillPersonalInformation;
 import progressivePages.FillVehicleDetails;
 import progressivePages.HomePage;
@@ -15,6 +16,7 @@ public class GetByQouteTest {
 	static WebDriver driver;
 	FillPersonalInformation fillPI;
 	FillVehicleDetails fillVehicleDetails;
+	FillDriverDetails fillDriverDetails;
 
 	@BeforeTest
 	public void setUpTest() {
@@ -41,7 +43,7 @@ public class GetByQouteTest {
 	public void sendZipCode() throws InterruptedException {
 		EnterZipCode enterZip = new EnterZipCode(driver);
 		enterZip.enterZipCode("75038");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 	}
 
 	@Test(priority = 2)
@@ -56,7 +58,6 @@ public class GetByQouteTest {
 		fillPI.enterApartmentNo("2149");
 		fillPI.selectPOBoxMilitiry();
 		fillPI.enterStreetAddress("3143 W Walnut Hill Lane");
-
 		fillPI.clickStartMyQuote();
 		Thread.sleep(4000);
 	}
@@ -67,24 +68,52 @@ public class GetByQouteTest {
 		fillVehicleDetails = new FillVehicleDetails(driver);
 
 		fillVehicleDetails.chooseVehicleYear("2019");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.chooseVehicleMake("Audi");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.chooseVehicleModel("A6");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.chooseBodyType("4DR 4CYL");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.choosePrimaryUse("Personal (to/from work or school, errands, pleasure)");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.chooseOwnOrLease("Own");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.chooseOwnPeriod("1 year - 3 years");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.chooseBlindSpotWarning();
 		fillVehicleDetails.clickOnDone();
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		fillVehicleDetails.continueToNextPage();
+		Thread.sleep(2000);
+	}
 
+	@Test(priority = 4)
+	public void fillDriverInformation() throws InterruptedException {
+		fillDriverDetails = new FillDriverDetails(driver);
+
+		fillDriverDetails.chooseGender();
+		fillDriverDetails.chooseMaritalStatus("Single");
+		fillDriverDetails.chooseLevelOfEducation("College degree");
+		fillDriverDetails.chooseEmployeementStatus("Employed");
+		Thread.sleep(2000);
+		fillDriverDetails.driverOccupation("Software Developer");
+		fillDriverDetails.driverSSN("867234789");
+		Thread.sleep(2000);
+		fillDriverDetails.choosePrimaryResidence("Rent");
+		fillDriverDetails.chooseLatestMoved("No");
+		Thread.sleep(2000);
+		fillDriverDetails.chooseLicenseStatus("Valid");
+		fillDriverDetails.chooseYearLicensed("3 years or more");
+		fillDriverDetails.clickAccidentClaimOrNot();
+		fillDriverDetails.clickTicketVoilationOrNot();
+		Thread.sleep(2000);
+		fillDriverDetails.clickContinueForNextPage();
+		Thread.sleep(4000);
+		fillDriverDetails.clickContinueToReviewPage();
+		Thread.sleep(4000);
+		fillDriverDetails.clickContinueToFinalDetailPage();
+		Thread.sleep(4000);
 	}
 
 //	@AfterTest
